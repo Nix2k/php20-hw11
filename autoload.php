1d82f98b6266ec4be74499b670aaad9ff1c7a4db
Nix2k <mihail.bokov@gmail.com>
2018-03-03 18:39:37 +0300

    v0.2

--- autoload.php
+++ autoload.php
@@ -1,8 +1,9 @@
 <?php
 	spl_autoload_register(
 		function($className){
-			if (file_exists("./classes/$className.php")) {
-				require_once "./classes/$className.php";
+			$fileName = str_replace('\\', DIRECTORY_SEPARATOR, $className);
+			if (file_exists("./classes/$fileName.php")) {
+				require_once "./classes/$fileName.php";
 			}
 			else {
 				die ("Класс $className не существует");
